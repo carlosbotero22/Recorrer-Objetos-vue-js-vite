2a45cdc4efb503114fda13a3451c9c8a50f98e71
carlosbotero22 <102599195+carlosbotero22@users.noreply.github.com>
2023-02-28 12:23:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Recorrer-Objetos-VUE-JS-VITE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-n5cvqr)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ name }}</h1>
+  <!-- recorrer objeto -->
+  <ul>
+      <li v-for="(value, propiedad) in objetoFruta" :key="value">
+          {{ propiedad }} : {{ value }}
+      </li>
+  </ul>
+</template>
+
+<script setup>
+const name = 'Vite + Vue';
+const objetoFruta = {
+  id: '1',
+  name: 'piña',
+  precio: '2.000',
+  descripcion: 'pina amarilla',
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
